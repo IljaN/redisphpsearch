73582bafb354cee67d9906b1c8631946bb2d1715
Ilja Neumann <node512@gmail.com>
2015-04-18 08:28:35 +0200

    Adding missing classes to bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,9 @@
 <?php
 include_once(__DIR__  . '/../src/IljaN/RedisPhpSearch/Search.php');
+include_once(__DIR__ . '/../src/IljaN/RedisPhpSearch/Indexer.php');
 include_once(__DIR__  . '/../src/IljaN/RedisPhpSearch/ClientInterface.php');
 include_once(__DIR__ . '/../src/IljaN/RedisPhpSearch/TransformerInterface.php');
-include_once(__DIR__  . '/../src/IljaN/RedisPhpSearch/Indexer.php');
-
 include_once(__DIR__  . '/../src/IljaN/RedisPhpSearch/TokenizerInterface.php');
 include_once(__DIR__  . '/../src/IljaN/RedisPhpSearch/Tokenizer/BasicTokenizer.php');
+include_once(__DIR__ . '/../src/IljaN/RedisPhpSearch/Transformer/Term/BasicTermTransformer.php');
+include_once(__DIR__ . '/../src/IljaN/RedisPhpSearch/ClientWrapper/PhpRedis.php');
