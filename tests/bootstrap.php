2da4275198c9ad7a607d388fb79ca9c75454464d
Ilja Neumann <node512@gmail.com>
2015-04-24 20:05:29 +0200

    Reordered bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,14 @@
 <?php
 include_once(__DIR__  . '/../src/IljaN/RedisPhpSearch/Search.php');
 include_once(__DIR__ . '/../src/IljaN/RedisPhpSearch/Indexer.php');
+
 include_once(__DIR__  . '/../src/IljaN/RedisPhpSearch/ClientInterface.php');
+include_once(__DIR__ . '/../src/IljaN/RedisPhpSearch/ClientWrapper/PhpRedis.php');
+
 include_once(__DIR__ . '/../src/IljaN/RedisPhpSearch/TransformerInterface.php');
+include_once(__DIR__ . '/../src/IljaN/RedisPhpSearch/Transformer/Term/SimpleTermTransformer.php');
+
 include_once(__DIR__  . '/../src/IljaN/RedisPhpSearch/TokenizerInterface.php');
 include_once(__DIR__ . '/../src/IljaN/RedisPhpSearch/Tokenizer/SimpleTokenizer.php');
-include_once(__DIR__ . '/../src/IljaN/RedisPhpSearch/Transformer/Term/SimpleTermTransformer.php');
-include_once(__DIR__ . '/../src/IljaN/RedisPhpSearch/ClientWrapper/PhpRedis.php');
+
+
